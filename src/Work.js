9907be46f090b5230764a89a09c0d28a0228c649
Mohammad <mohammadidehgamwala5@gmail.com>
2020-12-09 21:13:03 +0530

    added rel

--- src/Work.js
+++ src/Work.js
@@ -1,55 +1,48 @@
 import React from 'react';
 
 const Work = () => {
+  const workNames = [
+    {
+      title: 'Expager',
+      subtitle: 'Track your income and expense with visual data',
+      link: 'https://adoring-heyrovsky-15ffdd.netlify.app/',
+    },
+    {
+      title: 'KeepItShort',
+      subtitle: 'Be more productive by adding short notes',
+      link: 'https://laughing-poitras-8586b3.netlify.app/',
+    },
+    {
+      title: 'Cakes',
+      subtitle: 'A static cake shop with cart for your events',
+      link: 'https://elegant-shannon-a33fb2.netlify.app/',
+    },
+    {
+      title: 'Socialize',
+      subtitle: 'Post your creative thoughts and inspire others',
+      link: 'https://mohammad-socialize.netlify.app/',
+    },
+    {
+      title: 'Search Github Users',
+      subtitle: 'Search and analyze github users with visual data',
+      link: 'https://gifted-blackwell-a3dad8.netlify.app/',
+    },
+  ];
   return (
-    <section class="section work" id="work">
-      <h2 class="section-title">Work</h2>
-      <div class="work__container bd-grid">
-        <div class="work__1 work__names">
-          <div>
-            <h6>Expager</h6>
-            <p>Track your income and expense with visual data</p>
-            <a href="https://adoring-heyrovsky-15ffdd.netlify.app/" target="_blank">
-              Click to open
-            </a>
+    <section className="section work" id="work">
+      <h2 className="section-title">Work</h2>
+      <div className="work__container bd-grid">
+        {workNames.map(({ title, subtitle, link }) => (
+          <div className="work__names">
+            <div>
+              <h6>{title}</h6>
+              <p>{subtitle}</p>
+              <a href={link} target="_blank" rel="noreferrer">
+                Click to open
+              </a>
+            </div>
           </div>
-        </div>
-        <div class="work__2 work__names">
-          <div>
-            <h6>KeepItShort</h6>
-            <p>Be more productive by adding short notes</p>
-            <a href="https://laughing-poitras-8586b3.netlify.app/" target="_blank">
-              Click to open
-            </a>
-          </div>
-        </div>
-        <div class="work__3 work__names">
-          <div>
-            <h6>Cakes</h6>
-            <p>A static cake shop with cart for your events</p>
-            <a href="https://elegant-shannon-a33fb2.netlify.app/" target="_blank">
-              Click to open
-            </a>
-          </div>
-        </div>
-        <div class="work__4 work__names">
-          <div>
-            <h6>Socialize</h6>
-            <p>Post your creative thoughts and inspire others</p>
-            <a href="https://mohammad-socialize.netlify.app/" target="_blank">
-              Click to open
-            </a>
-          </div>
-        </div>
-        <div class="work__5 work__names">
-          <div>
-            <h6>Search Github Users</h6>
-            <p>Search and analyze github users with visual data</p>
-            <a href="https://gifted-blackwell-a3dad8.netlify.app/" target="_blank">
-              Click to open
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
